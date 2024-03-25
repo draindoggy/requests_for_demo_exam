06e7114aaa76ec88c014396b83e1da2c824d8d4c
draindoggy <119752646+draindoggy@users.noreply.github.com>
2024-03-25 13:05:00 +0300

    Add files via upload

---
+++ login.cs
@@ -0,0 +1,27 @@
+﻿namespace zayavki
+{
+    public partial class login : Form
+    {
+        public login()
+        {
+            InitializeComponent();
+        }
+
+        string loginName = "user";
+        string password = "12345";
+        public string? textBoxLogin { get; set; }
+        public string? textBoxPassword { get; set; }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBoxLogin = textBox1.Text;
+            textBoxPassword = textBox2.Text;
+
+            if (textBoxLogin == loginName && textBoxPassword == password)
+            {
+                Form mainForm = new Form1();
+                mainForm.ShowDialog();
+            }
+        }
+    }
+}
