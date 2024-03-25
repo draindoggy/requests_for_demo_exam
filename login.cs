0c69c3cb61d440dd606043ef20ccd193cbad972e
draindoggy <119752646+draindoggy@users.noreply.github.com>
2024-03-26 00:42:39 +0300

    Update login.cs

--- login.cs
+++ login.cs
@@ -1,14 +1,31 @@
-﻿namespace zayavki
+namespace zayavki
 {
     public partial class login : Form
     {
+        public List<string[]> loginPasswordList1 = new List<string[]>();
+
         public login()
         {
             InitializeComponent();
+            LoadDataFromFile();
+        }
+        private void LoadDataFromFile()
+        {
+            if (File.Exists("login.txt"))
+            {
+                loginPasswordList1.Clear();
+                using (StreamReader reader = new StreamReader("login.txt"))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        string[] data = line.Split(',');
+                        loginPasswordList1.Add(data);
+                    }
+                }
+            }
         }
 
-        string loginName = "user";
-        string password = "12345";
         public string? textBoxLogin { get; set; }
         public string? textBoxPassword { get; set; }
 
@@ -17,11 +34,31 @@
             textBoxLogin = textBox1.Text;
             textBoxPassword = textBox2.Text;
 
-            if (textBoxLogin == loginName && textBoxPassword == password)
+            bool loginSuccess = false;
+
+            foreach (string[] loginData in loginPasswordList1)
+            {
+                if (textBoxLogin == loginData[0] && textBoxPassword == loginData[1])
+                {
+                    loginSuccess = true;
+                    break;
+                }
+            }
+
+            if (loginSuccess)
             {
                 Form mainForm = new Form1();
                 mainForm.ShowDialog();
             }
+            else
+            {
+                MessageBox.Show("неверные логин или пароль. пожалуйста, попробуйте снова.");
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form regForm = new registration();
+            regForm.ShowDialog();
         }
     }
 }
