50ec860e226821dfff6cd1e55c6517a7f12e4827
draindoggy <119752646+draindoggy@users.noreply.github.com>
2024-03-26 00:41:18 +0300

    Add files via upload

---
+++ registration.cs
@@ -0,0 +1,51 @@
+﻿namespace zayavki
+{
+    public partial class registration : Form
+    {
+        public List<string[]> loginPasswordList = new List<string[]>();
+        public string? loginTextBox { get; set; }
+        public string? passwordTextBox { get; set; }
+
+        public registration()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            loginTextBox = textBox1.Text;
+            passwordTextBox = textBox2.Text;
+
+            loginPasswordList.Add(new string[] { loginTextBox, passwordTextBox });
+
+            SaveDataToFile();
+        }
+
+        private void SaveDataToFile()
+        {
+            using (StreamWriter writer = new StreamWriter("login.txt"))
+            {
+                foreach (string[] data in loginPasswordList)
+                {
+                    writer.WriteLine(string.Join(",", data));
+                }
+            }
+        }
+        //private void LoadDataFromFile()
+        //{
+        //    if (File.Exists("login.txt"))
+        //    {
+        //        loginPasswordList.Clear();
+        //        using (StreamReader reader = new StreamReader("login.txt"))
+        //        {
+        //            string line;
+        //            while ((line = reader.ReadLine()) != null)
+        //            {
+        //                string[] data = line.Split(',');
+        //                loginPasswordList.Add(data);
+        //            }
+        //        }
+        //    }
+        //}
+    }
+}
